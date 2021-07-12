ad824a4ee1319135302726a3661ad6e65271a073
salva-dali2997 <salvatore1004@gmail.com>
2021-07-12 13:52:12 -0400

    Revert back to working version

--- PhoneBook/Classes/Address.cs
+++ PhoneBook/Classes/Address.cs
@@ -7,35 +7,42 @@ namespace PhoneBook.Classes
 {
     public class Address
     {
-        public string Street_Address_1 { get; set; }
-        public string Street_Address_2 { get; set; }
+        public string StreetName { get; set; }
+        public string AddressNumber { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string Zip_Code { get; set; }
+        public string ZipCode { get; set; }
 
 
         /// <summary>
         /// Constructs an address object with at least a 
         /// Street Name and an Address Number
         /// </summary>
-        /// <param name="streetAddress1"></param>
-        /// <param name="streetAddress2"></param>
+        /// <param name="streetName"></param>
+        /// <param name="addressNumber"></param>
         /// <param name="city"></param>
         /// <param name="state"></param>
         /// <param name="zipCode"></param>
-        public Address(string streetAddress1 = null, string streetAddress2 = null, string city = null, string state = null, string zipCode = null)
+        public Address(string streetName = null, string addressNumber = null, string city = null, string state = null, string zipCode = null)
         {
-            this.Street_Address_1 = streetAddress1;
-            this.Street_Address_1 = streetAddress2;
+            this.StreetName = streetName;
+            this.AddressNumber = addressNumber;
             this.City = city;
             this.State = state;
-            this.Zip_Code = zipCode;
+            this.ZipCode = zipCode;
         }
-        
+
+        /*
+        public Address(string streetName, string addressNumber) : this(streetName, addressNumber, "unknown", "Ohio", "")
+        {
+
+        }
+        */
+
         public string AddressToString()
         {
-            string addressToString = $"{this.Street_Address_1} {this.Street_Address_2}";
-            if(this.City != null)
+            string addressToString = $"{this.AddressNumber} {this.StreetName}";
+            if (this.City != null)
             {
                 addressToString += $"\n         {this.City}";
             }
@@ -45,7 +52,7 @@ namespace PhoneBook.Classes
             }
             if (this.City != null)
             {
-                addressToString += $" {this.Zip_Code}";
+                addressToString += $" {this.ZipCode}";
             }
             return addressToString;
         }
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
-using Microsoft.Extensions.Configuration;
+
 using PhoneBook.Classes;
-using PhoneBook.DAO;
 
 namespace PhoneBook
 {
@@ -12,21 +11,159 @@ namespace PhoneBook
     {
         static void Main(string[] args)
         {
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-            IConfigurationRoot configuration = builder.Build();
+            //Create a Dictionary that will store your contacts
+            Dictionary<string, Contact> contactListDictionary =
+                new Dictionary<string, Contact>();
 
-            string connectionString = configuration.GetConnectionString("PhoneBookProjects");
 
-            IContactDao contactDao = new ContactSqlDao(connectionString);
+            //File path for json file
+            string contactsJSON = @"C:\Users\Student\git\PhoneBook\PhoneBook\Contacts.json";
 
-            PhoneBookProjectsCLI application = new PhoneBookProjectsCLI(contactDao);
-            application.Run();
+            //If the file doesn't exist, create the file
+            if (!File.Exists(contactsJSON))
+            {
+                var myContactFile = File.Create(contactsJSON);
+                myContactFile.Close();
+            }
+            //If it does exist
+            else
+            {
+                //populate a dictionary with the current
+                //state of the json file
+                contactListDictionary = PopulateContactsDictionary(contactsJSON);
+            }
+
+
+            //Call the main menu method which prints a number selection
+            //menu to choose between a few options.
+            //1. Create a Contact
+            //2. Edit a contact
+            //3. Display a contact
+            //q. Quit
+            int userSelection = MainMenu();
+            while (true)
+            {
+                //User selected "Create a Contact"
+                if (userSelection == 1)
+                {
+                    //ask the user for contact information
+                    Contact contact = Program.PromptForContact();
+
+                    //add the contact to the current contact list dictionary
+                    contactListDictionary.Add(contact.ContactId, contact);
+
+                    //print success message, and then clear the console
+                    Console.WriteLine("Contact successfully added, press any button to continue:");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                }
+                //User selected "Edit a Contact"
+                else if (userSelection == 2)
+                {
+                    //Enter the contact list method
+                    PromptForContactEdit(contactListDictionary);
+
+                    //Clear the console
+                    Console.Clear();
+                }
+                //User selected "Display a Contact"
+                else if (userSelection == 3)
+                {
+                    //print the contacts to the console
+                    //from the contact list dictionary
+                    PrintContactsToConsole(contactListDictionary);
+
+                    //hold screen to see contacts, press button to continue
+                    Console.WriteLine("Press any button to continue:");
+                    Console.ReadLine();
+
+                    //Clear the console
+                    Console.Clear();
+                }
+                else if (userSelection == 0)
+                {
+                    //write it to json file
+                    File.WriteAllText(contactsJSON, JsonConvert.SerializeObject
+                        (contactListDictionary, Formatting.Indented));
+
+                    Console.WriteLine("Thanks! Have a Day.");
+                    break;
+                }
+                userSelection = MainMenu();
+            }
+
         }
 
 
+        /// <summary>
+        /// Populate the json contacts file into a Dictionary 
+        /// containing a string of unique Id Number
+        /// and a Contact object
+        /// </summary>
+        /// <param name="contactsJSON"></param>
+        /// <returns>a dictionary of string and contact</returns>
+        public static Dictionary<string, Contact> PopulateContactsDictionary(string contactsJSON)
+        {
+            //populate dictionary using streamreader and serializer
+            using (StreamReader file = new StreamReader(contactsJSON))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                Dictionary<string, Contact> contactsDictionary = (Dictionary<string, Contact>)serializer.Deserialize(file, typeof(Dictionary<string, Contact>));
+
+                //If the dictionary is null return a new initialized Dictionary
+                if (contactsDictionary == null)
+                {
+                    return new Dictionary<string, Contact>();
+                }
+
+                //otherwise return the populated dictionary
+                return contactsDictionary;
+            }
+        }
+
+        /// <summary>
+        /// Call the main menu method which prints a number selection 
+        /// menu to choose between a few options.
+        /// </summary>
+        /// <returns>integer of menu selection</returns>
+        static int MainMenu()
+        {
+            //print main menu
+            Console.WriteLine("Main Menu: ");
+            Console.WriteLine("[1] Create a Contact");
+            Console.WriteLine("[2] Edit a Contact");
+            Console.WriteLine("[3] Display a Contact");
+            Console.WriteLine("[q] to quit");
+            Console.Write("Select a number to continue:");
+
+            //ensure user input is valid. which at this point is
+            //1, 2, 3, or 'q'
+            while (true)
+            {
+                try
+                {
+                    string userInput = Console.ReadLine();
+                    if (userInput == "q")
+                    {
+                        return 0;
+                    }
+                    int entry = int.Parse(userInput);
+                    if (entry != 1 && entry != 2 && entry != 3)
+                    {
+                        Console.WriteLine("Please enter an accepted selection");
+                        continue;
+                    }
+                    return entry;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter an accepted selection");
+                    continue;
+                }
+            }
+        }
 
         /// <summary>
         /// If the user selects 1 in the main menu,
@@ -47,6 +184,7 @@ namespace PhoneBook
             Contact contact = new Contact(firstName, lastName, phoneNumber);
             return contact;
         }
+
         /// <summary>
         /// If the user selects 2 in the main menu,
         /// they are prompted to choose a contact to edit.
@@ -90,6 +228,8 @@ namespace PhoneBook
             }
         }
 
+
+
         /// <summary>
         /// If the user selects a 2 in the main menu,
         /// they are prompted to find the contact they
@@ -102,11 +242,11 @@ namespace PhoneBook
             {
                 Console.Clear();
                 Console.WriteLine("Which value would you like to edit?:");
-                Console.WriteLine($"[1]: {contactToEdit.First_Name}");
-                Console.WriteLine($"[2]: {contactToEdit.Last_Name}");
-                Console.WriteLine($"[3]: {contactToEdit.Phone_Number}");
+                Console.WriteLine($"[1]: {contactToEdit.FirstName}");
+                Console.WriteLine($"[2]: {contactToEdit.LastName}");
+                Console.WriteLine($"[3]: {contactToEdit.PhoneNumber}");
                 Console.WriteLine($"[4]: {contactToEdit.Email}");
-                Console.WriteLine($"[5]: {contactToEdit.Date_Of_Birth}");
+                Console.WriteLine($"[5]: {contactToEdit.DateOfBirth}");
                 string userInput = Console.ReadLine();
                 try
                 {
@@ -119,5 +259,76 @@ namespace PhoneBook
                 }
             }
         }
+
+        /// <summary>
+        /// If the user selects a 3 in the main menu,
+        /// the contents of the json file is printed 
+        /// to the console in a readable format
+        /// </summary>
+        /// <param name="contactsJSON"></param>
+        public static void PrintContactsToConsole(Dictionary<string, Contact> contactListDictionary)
+        {
+            foreach (KeyValuePair<string, Contact> kvp in contactListDictionary)
+            {
+                Console.WriteLine($"Name: {kvp.Value.FullName}");
+                Console.WriteLine($"Phone Number: {kvp.Value.PhoneNumber}");
+                Console.WriteLine($"Email: {kvp.Value.Email}");
+                Console.WriteLine($"DOB: {kvp.Value.DateOfBirth}");
+                Console.WriteLine($"Address: {kvp.Value.Address.AddressToString()}");
+            }
+        }
+
+
+        /// <summary>
+        /// This method reads in information from a csv and 
+        /// places them in a contact object. Which is then
+        /// placed into a json object.
+        /// </summary>
+        public static void ReadContactFromCSV()
+        {
+            string fileToRead = @"C:\Users\Student\git\git_testproject\" +
+                                    @"PhoneBook\PhoneBook\Contacts.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileToRead))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File could not be read");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Writes a specific contact to a CSV file
+        /// </summary>
+        /// <param name="contactToAppend"></param>
+        public static void WriteContactToFile(Contact contactToAppend)
+        {
+            string newFilName = @"C:\Users\Student\git\git_testproject\
+                                PhoneBook\PhoneBook\TextFiles\Contacts.txt";
+            try
+            {
+                using (StreamWriter sr = new StreamWriter(newFilName, true))
+                {
+                    sr.WriteLine($"{contactToAppend.FullName},{contactToAppend.PhoneNumber},{contactToAppend.Email}," +
+                        $"{contactToAppend.DateOfBirth}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File could not be read");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
     }
 }
